6151fe5d4a44412a22ffae4c8f3b09f0860e22c9
semteX <Klaus.Gradinger@gmail.com>
2024-01-20 19:57:29 +0100

    changed everything

---
+++ src/DustTracker.h
@@ -0,0 +1,41 @@
+#ifndef DUST_TRACKER_H
+#include <Arduino.h>
+#include <SdsDustSensor.h>
+#include <State.h>
+
+class DustTracker
+{
+private:
+    SdsDustSensor* dustSensor;
+    State* state;
+    long lastUpdate = 0;
+public:
+    DustTracker(State* state);
+    ~DustTracker();
+    void onTick();
+};
+
+DustTracker::DustTracker(State* state) {
+  dustSensor = new SdsDustSensor(Serial2);
+  dustSensor->begin();
+  this->state = state;
+  Serial.println(dustSensor->queryFirmwareVersion().toString()); 
+  Serial.println(dustSensor->setQueryReportingMode().toString());
+}
+
+DustTracker::~DustTracker() {
+}
+
+void DustTracker::onTick() {
+    if(millis() - lastUpdate > 2000) {
+        PmResult result = dustSensor->queryPm();
+        Serial.printf("PM2.5: %f, PM10: %f\n\n", result.pm25, result.pm10);
+        state->airQuality.pm10 = result.pm10;
+        state->airQuality.pm2_5 = result.pm25;
+        lastUpdate = millis();
+    }
+}
+
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,41 @@
 #include <Arduino.h>
 #include <MachineController.h>
-#include <LedTracker.h>
 #include <State.h>
 #include <WebServer.h>
 #include <WlanManager.h>
 #include <CredentialsManager.h>
 #include <Webserver.h>
+#include <DustTracker.h>
+#include <InputManager.h>
 
 State* state;
-LedTracker* ledTracker;
 MachineController* machineController;
 WlanManager* wlanManager;
 CredentialsManager* credManager;
 Webserver* webserver;
+DustTracker* dustTracker;
+InputManager* inputManager;
+
 
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n\n");
   state = new State();
   credManager = new CredentialsManager();
-  ledTracker = new LedTracker(GPIO_NUM_32, GPIO_NUM_33, GPIO_NUM_35, state);
-  machineController = new MachineController(GPIO_NUM_20, GPIO_NUM_21, state);
+  credManager->writeFile(SSID_FILE, "");
+  credManager->writeFile(PASSWORD, "");
+  machineController = new MachineController(GPIO_NUM_25, GPIO_NUM_26, state);
   wlanManager = new WlanManager(credManager);
   wlanManager->start();
   webserver = new Webserver(credManager, machineController, state);
   webserver->start();
+  dustTracker = new DustTracker(state);
+  inputManager = new InputManager(GPIO_NUM_12, GPIO_NUM_14, state);
 }
 
 void loop() {
-  ledTracker->onTick();
   machineController->onTick();
+  dustTracker->onTick(); 
+  inputManager->tick();
+  delay(50);
 }
